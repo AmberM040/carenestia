5681c80e3af9dfcc91f05a5dc60ffea4ebdd7ca6
AmberM040 <support@carenestia.com>
2026-03-07 23:08:04 +0000

    Add files via upload

---
+++ carenestprototype/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://jtdequkcehkvwibxzszm.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_yi8w78I0laGFmm8OizVTLA_F3HhMznO";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
